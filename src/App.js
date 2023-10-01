244f055759ee134d42105cbb87998a6732ac9ca3
divyesh2123 <divyesh2198@gmail.com>
2023-10-02 04:52:45 +0530

    birth Day remainder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+import List from './List'
+function App() {
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+       
+      </section>
+    </main>
+  )
+}
+
+export default App
